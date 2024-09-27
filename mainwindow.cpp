e5bf6a3a63c52f9a226169f0a07eaf63b805e731
Youenn <156934138+Monsteryou@users.noreply.github.com>
2024-09-27 12:27:50 +0200

    troisieme commit
    partie SHA

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,9 @@
 #include "AesGestion.h"
 #include "RsaGestion.h"
 #include <iostream>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QFile>
 
 using namespace std;
 
@@ -19,48 +22,52 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
 void MainWindow::on_btn_chif_aes_clicked()
 {
-
-
+    // Implémentation à ajouter
 }
 
 void MainWindow::on_btn_dechif_aes_clicked()
 {
-
-
+    // Implémentation à ajouter
 }
-
 
 void MainWindow::on_btn_chif_rsa_clicked()
 {
-
-
+    // Implémentation à ajouter
 }
-
 
 void MainWindow::on_btn_dechif_rsa_clicked()
 {
-
     RsaGestion RSA;
-
+    // Implémentation à ajouter
 }
 
 void MainWindow::on_btn_sha_clicked()
 {
-    HashGestion SHA;
-    cout << SHA.CalculateSHA256("c'est un chat") << endl;
+    QString filePath = QFileDialog::getOpenFileName(this, "Sélectionner un fichier", "", "Tous les fichiers (*.*)");
+    if (filePath.isEmpty()) return;
+
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly)) {
+        QMessageBox::warning(this, "Erreur", "Impossible d'ouvrir le fichier.");
+        return;
+    }
+
+    QByteArray fileData = file.readAll();
+    file.close();
+
+    if (fileData.isEmpty()) {
+
+        return;
+    }
+
+    try {
+        HashGestion SHA;
+        QString hash = QString::fromStdString(SHA.CalculateSHA256(string(fileData.constData(), fileData.length())));
+        cout << "Hash SHA-256 du fichier : " << hash.toStdString() << endl;
+        QMessageBox::information(this, "Hash SHA-256", "Le hash SHA-256 du fichier est :\n" + hash);
+    } catch (const exception& e) {
+
+    }
 }
-
-void MainWindow::on_btn_chif_aes_2_clicked()
-{
-
-}
-
-
-void MainWindow::on_btn_selec_clicked()
-{
-
-}
-
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -13,36 +12,19 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+public slots:
 
+    void on_btn_chif_aes_clicked();
+    void on_btn_dechif_aes_clicked();
+    void on_btn_chif_rsa_clicked();
+    void on_btn_dechif_rsa_clicked();
+    void on_btn_sha_clicked();
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void on_btn_sha_clicked();
-    void on_btn_chif_aes_clicked();
-    void on_btn_chif_rsa_clicked();
-    void on_btn_dechif_aes_clicked();
-    void on_btn_dechif_rsa_clicked();
-    void on_btn_chif_aes_2_clicked();
-    void on_btn_selec_clicked();
 
 private:
     Ui::MainWindow *ui;
 };
-
-class mafenetre : public QWidget // On hérite de QWidget
-{
-    Q_OBJECT
-
-public:
-    mafenetre();
-
-public slots:
-    void ouvrirDialogue();
-
-private:
-    QPushButton *boutonDialogue;
-};
-
 #endif // MAINWINDOW_H

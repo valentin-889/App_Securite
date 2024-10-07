34b4eb1a90d85db790869e29ce7bfe422715c691
Youenn <156934138+Monsteryou@users.noreply.github.com>
2024-10-07 21:04:04 +0200

    doxygene
    ajouts de commentaire main cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
-#include "mainwindow.h"
+/**
+ * Fonction principale de l'application.
+ * 
+ * Initialise l'application Qt et affiche la fenêtre principale.
+ */
 
+#include "mainwindow.h"
 #include <QApplication>
 
+/**
+ * Point d'entrée de l'application.
+ * 
+ * Crée et affiche la fenêtre principale, puis lance la boucle d'événements Qt.
+ * 
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
